2e1cfe16ac45d98c22c0da4bc0b232ea2abc745c
Makoto Watanabe <mktwtnb1989@gmail.com>
2020-05-06 18:31:46 +0100

    Completed Section-13 No.209

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,5 @@
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,8 +9,39 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({ recipients: false });
+
+    res.send(surveys)
+  });
+
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    req.body.map(({email, url}) => {
+      const match = p.test(new URL(url).pathname);
+      if (match) {
+        return {email, surveyId: match.surveyId, choice: match.choice};
+      }
+    })
+    .filter(Boolean)
+    .filter((x, idx, self) => {return (self.findIndex(value => value.email == x.email && value.surveyId == x.surveyId) === idx)})
+    .forEach(({ surveyId, email, choice }) => {
+      Survey.updateOne({
+        _id: surveyId,
+        recipients: {
+          $elemMatch: { email: email, responded: false }
+        }
+      }, {
+        $inc: { [choice]: 1 },
+        $set: { 'recipients.$.responded': true },
+        lastResponded: new Date()
+      }).exec();
+    });
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
